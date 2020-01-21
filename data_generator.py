b171d22b2c2db05e4721317a715f3d42074251d2
Colin Fitzgerald <cfitzgerald@plantiga.com>
2020-01-21 14:20:56 -0800

    Basic test to see if everything is working.  No real features yet.

---
+++ data_generator.py
@@ -0,0 +1,38 @@
+import psycopg2
+from pprint import pprint
+from time import time
+from datetime import datetime
+import threading
+from math import sin
+
+data_source_context = {'host': '192.168.2.178',
+                       'dbname': 'pi',
+                       'user': 'pi',
+                       'password': 'pi'}
+
+DEVICE_ID = 'D2000-00001'
+
+conn = psycopg2.connect(**data_source_context)
+
+
+def inject_data():
+    with conn:
+        cur = conn.cursor()
+
+        u = time()
+        pm25 = (sin(u / 60) + 1)*100
+
+        print(u, pm25)
+
+        with cur:
+            cur.execute("INSERT INTO telemetry (device_id, timestamp, pm25)"
+                        "VALUES (%s, %s, %s)", (DEVICE_ID, datetime.now(), pm25))
+
+
+if __name__ == '__main__':
+
+    timer = threading.Event()
+
+    while not timer.wait(1):
+        inject_data()
+
